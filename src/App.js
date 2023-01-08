c31104ba28e7f2412f9e5ef1c4f56482947df5a4
pogruzhenie <122218374+pogruzhenie@users.noreply.github.com>
2023-01-08 19:01:17 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,12 +22,11 @@ const App = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>{message}</p>
-        <p>Change me!</p>
+        <p>Изменено</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
 
         </p>
-        <p>Отредактировали</p>
         <a
           className="App-link"
           href="https://reactjs.org"
